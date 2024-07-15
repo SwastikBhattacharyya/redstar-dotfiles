d0b7fe8e625428fab9be3bbe62c4e3e733ac2bdb
Swastik Bhattacharyya <swastikunity@gmail.com>
2024-07-15 13:28:21 +0530

    Add neovim configuration file with colorschemes and basic plugins
    This commit adds the init.lua file, with the configured tabs and spaces,
    along with the gruvbox color scheme. The telescope and treesitter
    plugins have also been installed and configured.

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,86 @@
+-- Tabs and Spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- Set mapleader
+vim.g.mapleader = " "
+
+-- Package Manager (lazy.nvim)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Loading Plugins
+local plugins = {
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
+local opts = {}
+
+require("lazy").setup({
+    plugins,
+    opts
+})
+
+-- Plugins Setup
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- Gruvbox
+require("gruvbox").setup({
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = true,
+})
+vim.cmd("colorscheme gruvbox")
+
+-- Treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = {"lua"},
+    highlight = { enable = true },
+    indent = { enable = true }
+})
+
