9e69a9b58e90f62d6dcaad79a1ea2b904e0b3051
Swastik Bhattacharyya <swastikunity@gmail.com>
2024-07-28 21:39:10 +0530

    Update bufferline and colorscheme configuration for neovim
    This commit updates the bufferline and colorscheme configuration for
    neovim. The bufferline style has been changed to tabbed and the
    colorscheme configuration toggles transparency based on if it is opened
    in neovim or neovide.

--- neovim/.config/nvim/lua/rebel_dev/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/rebel_dev/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-        require("gruvbox").setup({
-            contrast = "hard",
-            transparent_mode = true
-        })
-        vim.cmd("colorscheme gruvbox")
-    end
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		if vim.g.neovide then
+			require("gruvbox").setup({
+				contrast = "hard",
+				transparent_mode = false,
+			})
+		else
+			require("gruvbox").setup({
+				contrast = "hard",
+				transparent_mode = true,
+			})
+		end
+		vim.cmd("colorscheme gruvbox")
+	end,
 }
-
